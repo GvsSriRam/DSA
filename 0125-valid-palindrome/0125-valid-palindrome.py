74e8d98431b8361a0da060772f0bd1803e9cd981
GVS SRI RAM <57980789+GvsSriRam@users.noreply.github.com>
2024-12-18 01:59:25 -0500

    Time: 15 ms (14.44%), Space: 17.5 MB (44.14%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,25 @@
+class Solution:
+    def is_valid_char(self, c: str) -> bool:
+        if not c.isalnum():
+            return False
+        return True
+    
+    def isPalindrome(self, s: str) -> bool:
+        l = 0
+        r = len(s) - 1
+
+        while l<=r:
+            if not self.is_valid_char(s[l]):
+                l += 1
+                continue
+            
+            if not self.is_valid_char(s[r]):
+                r -= 1
+                continue
+            
+            if not s[l].lower() == s[r].lower():
+                return False
+
+            l += 1
+            r -= 1
+        return True
