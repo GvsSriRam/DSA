be72c1de331f76aedaf1cd6fe4524e201c0d0d59
GVS SRI RAM <57980789+GvsSriRam@users.noreply.github.com>
2025-06-06 14:26:51 -0400

    Time: 419 ms (80.46%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,5 +1,20 @@
 # Write your MySQL query statement below
-SELECT a.id
-FROM Weather a
-INNER JOIN Weather b ON DATEDIFF(a.recordDate, b.recordDate) = 1
-WHERE a.temperature > b.temperature
+WITH LagFeatData AS
+(
+    SELECT
+        id,
+        recordDate,
+        temperature,
+        LAG(temperature, 1) OVER (ORDER BY recordDate) AS PrevDayTemp,
+        LAG(recordDate, 1) OVER (ORDER BY recordDate) as PrevDate
+    FROM
+        Weather
+)
+SELECT
+    id
+FROM
+    LagFeatData
+WHERE
+    temperature > PrevDayTemp
+    AND
+    recordDate = DATE_ADD(prevDate, INTERVAL 1 DAY)
