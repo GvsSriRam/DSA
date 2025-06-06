a972c9246a44716e6b55982e4c2362165150f9d8
GVS SRI RAM <57980789+GvsSriRam@users.noreply.github.com>
2025-06-06 14:40:00 -0400

    Time: 525 ms (33.4%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -2,13 +2,8 @@
 SELECT
     w1.id
 FROM
-    Weather w1
+    Weather w1, Weather w2
 WHERE
-    w1.temperature > (
-        SELECT
-            w2.temperature
-        FROM
-            Weather w2
-        WHERE
-            w2.recordDate = DATE_SUB(w1.recordDate, INTERVAL 1 DAY)
-    )
+    DATEDIFF(w1.recordDate, w2.recordDate) = 1
+    AND
+    w1.temperature > w2.temperature
