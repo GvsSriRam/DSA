2d322e407d02345761097abd658db311cc99887e
GVS SRI RAM <57980789+GvsSriRam@users.noreply.github.com>
2025-06-06 00:34:18 -0400

    Time: 224 ms (5%), Space: 40.8 MB (7.69%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,14 +1,13 @@
+from collections import defaultdict
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         len_at_ele = defaultdict(int)
         res = 0
-
-        for num in nums:
-            if not len_at_ele[num]:
-                len_at_ele[num] = len_at_ele[num-1] + len_at_ele[num+1] + 1
-                len_at_ele[num - len_at_ele[num-1]] = len_at_ele[num]
-                len_at_ele[num + len_at_ele[num+1]] = len_at_ele[num]
-            
-            res = max(res, len_at_ele[num])
-        
+        for n in nums:
+            if not len_at_ele[n]:
+                len_at_ele[n] = len_at_ele[n-1] + len_at_ele[n+1] + 1
+                len_at_ele[n - len_at_ele[n-1]] = len_at_ele[n]
+                len_at_ele[n + len_at_ele[n+1]] = len_at_ele[n]
+            res = max(res, len_at_ele[n])
         return res
+
