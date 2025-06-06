e40cca5781dd3a4447b14696f0d2bd2ceb74f612
GVS SRI RAM <57980789+GvsSriRam@users.noreply.github.com>
2025-06-06 02:04:51 -0400

    Time: 0 ms (100%), Space: 18.1 MB (10.23%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,6 +1,8 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
+        if s[0] in ")}]":
+            return False
         for c in s:
             if c == ")" and len(stack) > 0 and stack[-1] == "(":
                 stack.pop()
