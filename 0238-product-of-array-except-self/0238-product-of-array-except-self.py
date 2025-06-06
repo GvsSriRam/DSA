c1dde5c5760cbfee885ef4db06c5997987562338
GVS SRI RAM <57980789+GvsSriRam@users.noreply.github.com>
2025-06-06 00:09:15 -0400

    Time: 25 ms (52.95%), Space: 23.3 MB (79.7%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,7 +1,6 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         res = [1]*len(nums)
-
         prefix = 1
         for i in range(1, len(nums)):
             prefix *= nums[i-1]
@@ -13,3 +12,4 @@ class Solution:
             res[i] *= suffix
         
         return res
+
