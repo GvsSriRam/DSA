76d0cdae4880b96f46bc4379f2f94345e1becdc6
GVS SRI RAM <57980789+GvsSriRam@users.noreply.github.com>
2026-02-22 18:09:04 -0800

    Time: 0 ms (100%), Space: 20.7 MB (16.93%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,9 +2,8 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         idx = dict()
         for i in range(len(nums)):
-            idx[nums[i]] = i
-        for i in range(len(nums)):
             search_val = target - nums[i]
-            if search_val in idx and idx[search_val] != i:
+            if search_val in idx:
                 return [i, idx[search_val]]
+            idx[nums[i]] = i
         return []
